e655d7ffafff0bbf5d61a2aff43d7b06c40b3829
Aleksandr Shman <sancho1741@mail.ru>
2022-04-29 02:05:00 +0300

    release coloborator

--- components/MainContainer.js
+++ components/MainContainer.js
@@ -6,13 +6,12 @@ import A from "../components/A"
 function MainContainer({ children, keywords }) {
  return (
   <>
-
    <Head>
     <meta keywords={'Все ключевые слова через запятую, Next.js' + keywords}></meta>
     <title>Главная</title>
    </Head>
-
    <div className={styles.nav}>
+
 
     <A href={'/'} text='Главная' />
     <A href={'/users'} text='Пользователи' />
