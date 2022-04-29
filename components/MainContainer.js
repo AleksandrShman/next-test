7c4592961c8c92b5f3c7fba56e2d40e8105fab78
Aleksandr Shman <sancho1741@mail.ru>
2022-04-29 12:12:18 +0300

    first

---
+++ components/MainContainer.js
@@ -0,0 +1,26 @@
+import Head from "next/head"
+import styles from '../styles/A.module.css'
+import A from "../components/A"
+
+
+function MainContainer({ children, keywords }) {
+ return (
+  <>
+   <Head>
+    <meta keywords={'Все ключевые слова через запятую, Next.js' + keywords}></meta>
+    <title>Главная</title>
+   </Head>
+   <div className={styles.nav}>
+
+
+    <A href={'/'} text='Главная' />
+    <A href={'/users'} text='Пользователи' />
+   </div>
+   <div>
+    {children}
+   </div>
+  </>
+ )
+}
+
+export default MainContainer
